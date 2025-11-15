6bcef68fcb9f0831bc803415bed4b8563c1fe7c0
Ansh-Sharma78 <anshsharma.0008@gmail.com>
2025-11-15 18:39:03 +0000

    datatypes summary

---
+++ 01_basics/05_datatypes_summary.js
@@ -0,0 +1,33 @@
+//primitive data types
+
+const score= 12 //dynamic typed laanguage
+
+const id=Symbol('123')  //symbol datatype
+const anotherid=Symbol('123')
+
+//console.log(id===anotherid); false
+//const bigNumber=3434340000000000
+
+//refrence(non-primitive)
+const temp=null
+const temp2=undefined
+//array
+const heroes=["ironman","cuteiman","spiderman"]; 
+
+//object
+let myObj = {
+    name:"ansh",
+    age: 22,
+}
+
+//function
+const myFunction = function(){
+// console.log("helo world");
+
+// }
+
+// console.log(typeof heroes);
+
+
+//++++++++++++++++++++++++++++++++++Memory in js+++++++++
+
